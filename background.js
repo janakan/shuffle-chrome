cbac0737456059d767df42715a92ae4ed6f4c12f
Janakan Arulkumarasan <janakan@onoko.com>
2017-10-20 14:00:12 +0800

    Version 0.0.5

--- background.js
+++ background.js
@@ -14,6 +14,21 @@ var subscriptions = [
   "wtf"
 ];
 
+var service = analytics.getService("boredom_button");
+var tracker = service.getTracker("UA-61221904-15");
+tracker.sendAppView("Launch");
+tracker.sendEvent("Chrome","Start");
+
+chrome.runtime.onInstalled.addListener(function(details){
+  if(details.reason == "install"){
+    tracker.sendEvent("Chrome","Install");
+  }
+  if(details.reason == "update"){
+    tracker.sendEvent("Chrome","Update");
+  }
+});
+chrome.runtime.setUninstallURL("http://www.boredombutton.com/uninstall.html?client=chrome");
+
 var items = {};
 var pending = [];
 
@@ -48,6 +63,7 @@ chrome.browserAction.onClicked.addListener(function(tab){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var currentTab = tabs[0];
     var item = randomItem();
+    tracker.sendEvent("Chrome","Shuffle","BrowserAction");
     chrome.tabs.update(currentTab.id, {url:item.url});
   });
 });
@@ -55,7 +71,11 @@ chrome.browserAction.onClicked.addListener(function(tab){
 chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
   switch(request.type){
     case "random":
+      tracker.sendEvent("Chrome","Shuffle","Mouse");
       sendResponse(randomItem());
+      break;
+    case "visit":
+      tracker.sendEvent("Chrome","Visit",request.hostname);
       break;
     default:
       console.log("Unknown request type",request);
--- inject.js
+++ inject.js
@@ -3,6 +3,8 @@ var progressBar = document.createElement("shuffle-progress");
 progressBar.innerHTML = "";
 document.body.appendChild(progressBar);
 
+chrome.runtime.sendMessage({type:"visit",hostname:window.location.hostname});
+
 function shuffle(){
   chrome.runtime.sendMessage({type:"random"},function(item){
     window.location.href = item.url;
@@ -15,7 +17,6 @@ function activate(e){
     i++;
     progressBar.style.visibility = "visible";
     progressBar.style.width = 20 + (i*2) + "%";
-    console.log("ACTIVATE!");
     if(i == 40){
       clearInterval(countup);
       active = false;
@@ -33,42 +34,16 @@ function reset(){
 window.addEventListener("mousedown",activate);
 window.addEventListener("mouseup",reset);
 window.addEventListener("mousemove",reset);
+window.addEventListener("click",reset);
 
 /*
-var shuffleCircle = document.createElement("shuffle-circle");
-shuffleCircle.innerHTML = ""
-document.body.appendChild(shuffleCircle);
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-61221904-15"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-var shuffleCover = document.createElement("shuffle-cover");
-shuffleCover.innerHTML = "";
-document.body.appendChild(shuffleCover);
-
-var expansion;
-window.addEventListener("mousedown",function(e){
-  var pointX = e.clientX;
-  var pointY = e.clientY;
-  shuffleCircle.style.left = pointX + "px";
-  shuffleCircle.style.top = pointY + "px";
-  var i = 0;
-  expansion = setInterval(function(){
-    i+=10;
-    shuffleCircle.style.left = (pointX-i) + "px";
-    shuffleCircle.style.top = (pointY-i) + "px";
-    shuffleCircle.style.width = shuffleCircle.style.height = (i*2) + "px";
-    console.log("Expand",i);
-    if(i*2 > window.innerWidth){
-      reset2();
-      shuffleCover.style.visibility = "visible";
-      shuffle();
-    }
-  },10);
-})
-
-function reset2(){
-  clearInterval(expansion);
-  shuffleCircle.style.width = 0 + "px";
-}
-
-window.addEventListener("mouseup",reset2);
-window.addEventListener("mousemove",reset2);
+  gtag('config', 'UA-61221904-15');
+</script>
 */
