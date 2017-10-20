274dbb367e6ac1592c72e4493b1f3b61ff4250e9
Janakan Arulkumarasan <janakan@onoko.com>
2017-10-20 11:01:13 +0800

    Version 0.0.4

--- background.js
+++ background.js
@@ -1,54 +1,53 @@
-console.log("Hello world");
-
-
-chrome.webRequest.onHeadersReceived.addListener(
-  function (details) {
-    for (var i = 0; i < details.responseHeaders.length; ++i) {
-      if (details.responseHeaders[i].name.toLowerCase() == 'x-frame-options') {
-        details.responseHeaders.splice(i, 1);
-        return {
-          responseHeaders: details.responseHeaders
-        };
-      }
-    }
-  }, {urls: ["<all_urls>"]}, ["blocking", "responseHeaders"]);
-
-
-
-
 var subscriptions = [
   "news",
   "funny",
   "videos",
   "pics",
-  "todayilearned"
+  "todayilearned",
+  "worldnews",
+  "gifs",
+  "aww",
+  "movies",
+  "mildlyinteresting",
+  "politics",
+  "nottheonion",
+  "wtf"
 ];
 
 var items = {};
+var pending = [];
 
-subscriptions.forEach(function(s){
-  fetch("https://api.reddit.com/r/" + s +"/hot").then(function(resp){
-    return resp.json();
-  }).then(function(resp){
-    resp.data.children.forEach(function(child){
-      var item = child.data;
-      items[item.id] = item;
+function update(){
+  subscriptions.forEach(function(s){
+    fetch("https://api.reddit.com/r/" + s + "/hot").then(function(resp){
+      return resp.json();
+    }).then(function(resp){
+      resp.data.children.forEach(function(child){
+        var item = child.data;
+        if(!items[item.id]){
+          pending.push(item);
+          items[item.id] = item;
+        }
+      })
     })
-    console.log(Object.keys(items).length,"items");
   })
-})
+}
+
+update();
 
 function randomItem(){
-  var keys = Object.keys(items);
-  var rkey = keys[Math.floor(Math.random() * keys.length)];
-  return items[rkey];
+  if(pending.length < 1){
+    pending = Object.values(items);
+  }
+  var rid = Math.floor(Math.random() * pending.length);
+  var si = pending.splice(rid,1)[0];
+  return si;
 }
 
 chrome.browserAction.onClicked.addListener(function(tab){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var currentTab = tabs[0];
     var item = randomItem();
-    console.log("Selected",item);
     chrome.tabs.update(currentTab.id, {url:item.url});
   });
 });
@@ -63,74 +62,3 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
       break;
   }
 })
-
-var i = 0;
-function updateIcon(){
-  var colors = ["yellow","red","blue","purple"]
-  i++;
-  var color = 'hsl(' + i + ', 100%, 50%)';
-  console.log(color);
-  var canvas = document.createElement("canvas");
-  canvas.width = 150;
-  canvas.height = 150;
-  var ctx = canvas.getContext('2d');
-
-  ctx.fillStyle = color;
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 5;
-  ctx.beginPath();
-  ctx.arc(75, 75, 70, 0, Math.PI * 2); // Outer circle
-  ctx.stroke();
-  ctx.fill();
-
-  ctx.fillStyle = "black";
-  ctx.strokeStyle = "black";
-  ctx.beginPath();
-  ctx.arc(50,60,10,0,Math.PI * 2);
-  ctx.fill();
-
-  ctx.fillStyle = "black";
-  ctx.strokeStyle = "black";
-  ctx.beginPath();
-  ctx.arc(100,60,10,0,Math.PI * 2);
-  ctx.fill();
-
-  ctx.lineWidth = 5;
-  ctx.beginPath();
-  ctx.moveTo(40,100);
-  ctx.lineTo(110,110);
-  ctx.stroke();
-
-  chrome.browserAction.setIcon({
-    imageData: ctx.getImageData(0, 0, 150, 150)
-  });
-}
-
-//setInterval(updateIcon,100);
-console.log("SUCCESS!");
-
-
-/*
-setTimeout(function(){
-  var item = randomItem();
-  if(item.thumbnail && item.thumbnail != "default"){
-    console.log("THUMB",item.thumbnail);
-    showNotification(item);
-  }else{
-    console.log("Doesn't have a thumbnail");
-  }
-},10000);
-
-function showNotification(item,imgurl){
-  chrome.notifications.create(item.id,{
-    type:"image",
-    iconUrl:item.thumbnail,
-    imageUrl:item.thumbnail,
-    title:item.title,
-    message:"Trending on BoredomButton",
-    isClickable:true
-  },function(notId){
-    console.log("OK",notId);
-  })
-}
-*/
--- inject.js
+++ inject.js
@@ -1,33 +1,74 @@
+var countup;
+var progressBar = document.createElement("shuffle-progress");
+progressBar.innerHTML = "";
+document.body.appendChild(progressBar);
+
 function shuffle(){
   chrome.runtime.sendMessage({type:"random"},function(item){
     window.location.href = item.url;
   });
 }
 
-var progressBar = document.createElement("shuffle-progress");
-progressBar.innerHTML = "Hold to shuffle";
-document.body.appendChild(progressBar);
-
-var countup;
-
-window.addEventListener("mousedown",function(e){
+function activate(e){
   var i = 0;
   countup = setInterval(function(){
     i++;
-    progressBar.style.display = "block";
+    progressBar.style.visibility = "visible";
     progressBar.style.width = 20 + (i*2) + "%";
+    console.log("ACTIVATE!");
     if(i == 40){
       clearInterval(countup);
       active = false;
       shuffle();
     }
   },15);
-});
-window.addEventListener("mouseup",function(){
+}
+
+function reset(){
   clearInterval(countup);
   progressBar.style.width = "20%";
-  progressBar.style.display = "none";
-});
+  progressBar.style.visibility = "hidden";
+}
 
-window.addEventListener("mousemove",resetInfo);
-window.addEventListener("scroll",resetInfo);
+window.addEventListener("mousedown",activate);
+window.addEventListener("mouseup",reset);
+window.addEventListener("mousemove",reset);
+
+/*
+var shuffleCircle = document.createElement("shuffle-circle");
+shuffleCircle.innerHTML = ""
+document.body.appendChild(shuffleCircle);
+
+var shuffleCover = document.createElement("shuffle-cover");
+shuffleCover.innerHTML = "";
+document.body.appendChild(shuffleCover);
+
+var expansion;
+window.addEventListener("mousedown",function(e){
+  var pointX = e.clientX;
+  var pointY = e.clientY;
+  shuffleCircle.style.left = pointX + "px";
+  shuffleCircle.style.top = pointY + "px";
+  var i = 0;
+  expansion = setInterval(function(){
+    i+=10;
+    shuffleCircle.style.left = (pointX-i) + "px";
+    shuffleCircle.style.top = (pointY-i) + "px";
+    shuffleCircle.style.width = shuffleCircle.style.height = (i*2) + "px";
+    console.log("Expand",i);
+    if(i*2 > window.innerWidth){
+      reset2();
+      shuffleCover.style.visibility = "visible";
+      shuffle();
+    }
+  },10);
+})
+
+function reset2(){
+  clearInterval(expansion);
+  shuffleCircle.style.width = 0 + "px";
+}
+
+window.addEventListener("mouseup",reset2);
+window.addEventListener("mousemove",reset2);
+*/
